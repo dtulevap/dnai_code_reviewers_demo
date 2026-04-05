944ca3b3f1ef0a7d397e609fce71ee4e1361affd
dtulevap <tuleva@progress.com>
2026-04-05 10:13:54 +0300

    Add is_returned flag to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,6 +8,7 @@ renamed as (
         user_id                         as customer_id,
         order_date,
         status,
+        case when status = 'returned' then true else false end as is_returned,
         amount                          as order_amount,
         _updated_at
     from source
